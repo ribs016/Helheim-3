8201b077a6e1a0301e8ebeddaa9678f11823e71e
ribs016 <00001085710592sp@al.educacao.sp.gov.br>
2024-09-22 23:45:55 -0300

    Add files via upload

---
+++ Helheim - Copia/js/typeIt.js
@@ -0,0 +1,53 @@
+new TypeIt("#text01", {
+    strings: "Você acorda e as primeiras coisas que vê são galhos, você se encontra numa espécie de floresta. Ao caminhar por essa estranha floresta, pode se ouvir sons assustadores vindo mais adiante",
+    speed: 50,
+    waitUntilVisible: true,
+}).go();
+
+new TypeIt("#text02", {
+    strings: 'Ao seguir você se depara com um homem amarrado à uma àrvore, ele então te avista e diz: "Ei!, Você, criança, pode me ajudar a me soltar da àrvore?"',
+    speed: 50,
+    waitUntilVisible: true,
+}).go();
+
+new TypeIt("#text03", {
+    strings: 'Então ele diz: "Muito obrigado, bom, agora que estou livre quero me encontrar com a rainha, só ela pode realizar meu desejo, quer vir junto A proposito, meu nome é Sten',
+    speed: 50,
+    waitUntilVisible: true,
+}).go();
+
+new TypeIt("#text05", {
+    strings: 'De repente sua visão escurece, mas logo surge uma faguilha de luz que se expande até tomar por completo sua visão. Você acorda',
+    speed: 50,
+    waitUntilVisible: true,
+}).go();
+
+new TypeIt("#text06", {
+    strings: 'Você se encolhe em posição fetal, chorando, o que te aguarda é só dor e sofrimento pela enternidade',
+    speed: 50,
+    waitUntilVisible: true,
+}).go();
+
+new TypeIt("#text07", {
+    strings: 'Você ouve o barulho, porém segue outra direção, encontrando um cemitério.',
+    speed: 50,
+    waitUntilVisible: true,
+}).go();
+
+new TypeIt("#text08", {
+    strings: 'Ao entrar, você parece reconhecer o lugar, porém vagamente. Contudo, algo te chama a atenção, um túmulo',
+    speed: 50,
+    waitUntilVisible: true,
+}).go();
+
+new TypeIt("#text09", {
+    strings: 'Ao investigar, você consegue enxegar o nome de uma menina, Hilda Dahl, ao ler tal nome, você começa a chorar. Você está morta',
+    speed: 50,
+    waitUntilVisible: true,
+}).go();
+
+new TypeIt("#text10", {
+    strings: 'Você continua seu caminho até se depara com um castelo, porém aparenta não ter ninguém, ao entrar avista um bela porém intimidadora moça, ela se apresenta: "Olá pobre criança, imagino o que venha fazer aqui, é uma escolha sem volta, ainda quer prosseguir?"',
+    speed: 50,
+    waitUntilVisible: true,
+}).go();
